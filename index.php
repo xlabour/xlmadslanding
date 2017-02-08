26ec8aa9ad4ae1de96bc498a3536059d37cfccef
XLabour <syarif.hidayatullah@gmail.com>
2017-02-08 17:54:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,28 +45,27 @@ error_reporting(0);
 <!-- Primary Page Layout
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 <div class="container">
-	<div class="row">
-		<center>
-			<div class="column" style="margin-top: 3%">
-				<div style="position:relative; overflow: show">
-					<img src="mbbindovision_smallbanner_top.jpg" />
-					<br /> <br />
-					<h6 style="color:#777; font-weight:bold;">Masukkan Data Anda:</h6>
-					<form action="./submit.php" method="post">
-						<input type="hidden" name="origin" value="<?php echo $_SERVER['HTTP_REFERER'];?>" autocomplete="off" />
-						<input type="hidden" name="lonlat" id="lonlat" value="" /> <!-- LonLat untuk kemudahan Kami dalam mengechek Coverage XL di tempat Anda -->
-						<input type="text" name="name" value="" placeholder="Nama" autocomplete="off"  autofocus/><br />
-						<input type="number" name="phone" value="" onkeypress="return event.charCode >= 48 && event.charCode <= 57" placeholder="No. Telp / HP" autocomplete="off" /><br />
-						<input type="text" name="address" value="" placeholder="Alamat Pemasangan Super Bundle" autocomplete="off" /><br />
-						<!--textarea name="address" rows="5" placeholder="Alamat Pemasangan Super Bundle" autocomplete="off"></textarea-->
-						<label><input id="sendlocation" type="checkbox" name="sendlocation" value="1" /> Kirim lokasi saya (<i><a href="javascript:showFAQ();">kenapa?</a></i>)</label>
-						<input type="Submit" name="submit" id="submit" value="Kirim" class="button button-primary"/>
-						<br /><br />
-						<span style="font-size:10pt;"><i>Penggunaan paket ini untuk area 4G LTE XL,<br />Cek lokasi instalasi perangkatmu <a target="_blank" href="https://4g.xl.co.id/coverage">disini</a></i></span>
-					</form>
-				</div>
-			</div>
-		</center>
+	<div class="column" style="margin-top: 3%">
+		<div style="position:relative; overflow: show">
+			<img width="100%" src="mbbindovision_smallbanner_top.jpg" />
+			<br /> <br />
+			<h6 style="text-align:center; color:#777; font-weight:bold;">Masukkan Data Anda:</h6>
+			<form action="./submit.php" method="post">
+				<input type="hidden" name="origin" value="<?php echo $_SERVER['HTTP_REFERER'];?>" autocomplete="off" />
+				<input type="hidden" name="lonlat" id="lonlat" value="" /> <!-- LonLat untuk kemudahan Kami dalam mengechek Coverage XL di tempat Anda -->
+				<label>Nama:</label>
+				<input class="goWidth" type="text" name="name" value="" placeholder="" autocomplete="off"  autofocus /><br />
+				<label>No. Telp / HP:</label>
+				<input class="goWidth" type="number" name="phone" value="" onkeypress="return event.charCode >= 48 && event.charCode <= 57" placeholder="" autocomplete="off" /><br />
+				<label>Alamat Pemasangan Super Bundle:</label>
+				<!--input type="text" name="address" value="" placeholder="" autocomplete="off" /><br /-->
+				<textarea class="goWidth" name="address" rows="5" placeholder="" autocomplete="off"></textarea><br />
+				<label><input id="sendlocation" type="checkbox" name="sendlocation" value="1" /> Kirim lokasi saya (<i><a href="javascript:showFAQ();">kenapa?</a></i>)</label>
+				<center><input type="Submit" name="submit" id="submit" value="Kirim" class="button button-primary"/></center>
+				<br /><br />
+				<center><span style="font-size:10pt;"><i>Penggunaan paket ini hanya untuk area 4G LTE XL, Silahkan cek lokasi instalasi perangkatmu <a target="_blank" href="https://4g.xl.co.id/coverage">disini</a></i></span></center>
+			</form>
+		</div>
 	</div>
 </div>
 
